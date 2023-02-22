237d24cbe3997c3dd7a525394c4668c4584cb89c
Triple Patte <triplepatte123@gmail.com>
2023-02-22 12:57:28 +0100

    22 fév pokémon

--- week2/pokedex/script.js
+++ week2/pokedex/script.js
@@ -1,1 +1,47 @@
 // Insert your code here
+for (let i =0;i<15;i++){  
+        fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+        .then(response => response.json())
+        .then(data => {               
+            const a = `
+            <div class="pokemon ${data.types[0].type.name}">
+                      <div class="imgContainer">
+                          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png" alt="${data.name}" />
+                      </div>
+                      <div class="info">
+                          <h3 class="name">${data.name}</h3>
+                          <span class="type">Type: <span>${data.types[0].type.name}</span></span>
+                      </div>
+                  </div>`           
+              document.querySelector('#pokemonContainer').innerHTML += a; 
+          }
+        );
+}
+
+document.querySelector("#next").addEventListener('click',function(){
+
+                let c = document.querySelectorAll('.imgContainer').length;
+                for (let i =c;i<c+15;i++){  
+                    fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+                    .then(response => response.json())
+                    .then(data => {               
+                        const a = `
+                        <div class="pokemon ${data.types[0].type.name}">
+                                  <div class="imgContainer">
+                                      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png" alt="${data.name}" />
+                                  </div>
+                                  <div class="info">
+                                      <h3 class="name">${data.name}</h3>
+                                      <span class="type">Type: <span>${data.types[0].type.name}</span></span>
+                                  </div>
+                              </div>`           
+                          document.querySelector('#pokemonContainer').innerHTML += a; 
+                      }
+                    );
+            }
+
+         
+        })
+
+
+
