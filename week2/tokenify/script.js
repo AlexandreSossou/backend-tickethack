96c0cce807843e0c377f4bc33a67d787373f3af9
Triple Patte <triplepatte123@gmail.com>
2023-02-23 12:50:22 +0100

    week 2

--- week2/tokenify/script.js
+++ week2/tokenify/script.js
@@ -4,7 +4,7 @@ console.log("hello")
 const randomJoke = fetch('curl -X "GET" "https://api.spotify.com/v1/artists/" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer BQDlA4EyFyfe6PBIGk4EmZmrIAJgMSfbTIAATUsQYqZA0CGZCyEC3M8jfw0xREbi2-Sx0IKImYS7tD7mqsfFAFZJqNfdkvYIuB6pp36ldhheCkAnw5XlZO3Kx0UV1AU8wBzEigtU1pGMU1bkzObpZU9Mh5rxfNvJzedR54mpEjiTNKE5mYjAEkqIIKIaLz8ruGbq')
  .then(response => response.json())
  .then(data => {
-   console.log(data[artists].name);
+  
    //document.querySelector('#searchBar').value = 
    return data;
  });
