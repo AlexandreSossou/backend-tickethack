ce3d9e09e793c7ebf819934f9864592c8b17f219
Triple Patte <triplepatte123@gmail.com>
2023-02-23 14:46:53 +0100

    cb

--- week2/blackboard-part1/app.js
+++ week2/blackboard-part1/app.js
@@ -1,13 +1,13 @@
 const db = require('./database/setup'); // Do not edit/remove this line
 
-
+//test
 function displayAllArticles() {
 	db.find().then(data => {
 		console.log(data);
 	  });
 }
 
-
+//test
 function displayArticleByName(articleName) {
 	db.findOne({"name": articleName })
  .then(data => {
@@ -15,7 +15,7 @@ function displayArticleByName(articleName) {
  });
 }
 
-
+//test
 function displayArticleByID(articleId) {
 	db.findOne({ "_id": articleId })
  .then(data => {
@@ -25,19 +25,19 @@ function displayArticleByID(articleId) {
 
 displayArticleByID('625ea9ca38d6a3776994651f');
 
-
+//test
 function updateArticlePrice(articleId, newPrice) {
 	db.updateOne({ "_id": articleId },{"price":newPrice})
  
  ;
 }
 
-
+//test
 function updateArticleStock(articleId, newStock) {
 	db.updateOne({ "_id": articleId },{"stock":newStock})
 }
 
-
+//test
 function resetStocks() {
 	
 	db.updateMany({ stock: 0 })
