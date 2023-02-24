b28a6672ad505cb950b0540d6924bc1835f87dc6
Triple Patte <triplepatte123@gmail.com>
2023-02-24 09:53:30 +0100

    v24 fév

--- week2/tokenify/script.js
+++ week2/tokenify/script.js
@@ -1,12 +1,34 @@
 // Code goes here...
 console.log("hello")
 
-const randomJoke = fetch('curl -X "GET" "https://api.spotify.com/v1/artists/" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer BQDlA4EyFyfe6PBIGk4EmZmrIAJgMSfbTIAATUsQYqZA0CGZCyEC3M8jfw0xREbi2-Sx0IKImYS7tD7mqsfFAFZJqNfdkvYIuB6pp36ldhheCkAnw5XlZO3Kx0UV1AU8wBzEigtU1pGMU1bkzObpZU9Mh5rxfNvJzedR54mpEjiTNKE5mYjAEkqIIKIaLz8ruGbq')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const randomJoke = fetch('')
  .then(response => response.json())
  .then(data => {
   
-   //document.querySelector('#searchBar').value = 
+   
    return data;
  });
 
  
+
+
+
+
+ //Mon accès : curl -X "GET" "https://api.spotify.com/v1/artists/" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer BQDlA4EyFyfe6PBIGk4EmZmrIAJgMSfbTIAATUsQYqZA0CGZCyEC3M8jfw0xREbi2-Sx0IKImYS7tD7mqsfFAFZJqNfdkvYIuB6pp36ldhheCkAnw5XlZO3Kx0UV1AU8wBzEigtU1pGMU1bkzObpZU9Mh5rxfNvJzedR54mpEjiTNKE5mYjAEkqIIKIaLz8ruGbq
