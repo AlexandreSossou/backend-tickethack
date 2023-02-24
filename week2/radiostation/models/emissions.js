5ea4869a8f209eb59fd595f68db5fc3cd7851e11
Triple Patte <triplepatte123@gmail.com>
2023-02-24 10:07:31 +0100

    1

--- week2/radiostation/models/emissions.js
+++ week2/radiostation/models/emissions.js
@@ -1,1 +1,19 @@
+const mongoose = require("mongoose")
+
 // Insert your code here
+
+const monSchema = mongoose.Schema({
+    discontinuated: Boolean,
+    mainAnimator: String,
+    name: String,
+    nbAnimators: Number,
+
+})
+
+
+const Emission = mongoose.model('emissions', monSchema);
+
+module.exports = Emission;
+
+
+
--- week2/radiostation/models/songs.js
+++ week2/radiostation/models/songs.js
@@ -1,1 +1,16 @@
 // Insert your code here
+const mongoose = require("mongoose")
+
+
+const shemaSong = mongoose.Schema({
+    annoying: Boolean,
+    artist: String,
+    creationDate: new Date(),
+    title: Number,
+
+})
+
+
+const Song = mongoose.model('songs', shemaSong);
+
+module.exports = Song;
