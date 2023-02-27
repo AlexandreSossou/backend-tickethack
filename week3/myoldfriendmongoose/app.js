aae0b98d9da559ea77066b3ee6b137ee54311b02
Triple Patte <triplepatte123@gmail.com>
2023-02-27 12:08:25 +0100

    313

--- week3/myoldfriendmongoose/app.js
+++ week3/myoldfriendmongoose/app.js
@@ -11,32 +11,54 @@ function createCountry(name, flagImg, currency, population) {
 	    currency: currency,
 	    population: population,
        });
+       newCountry.save().then(data => {
+        console.log(data);})
 
 }
 
 
 
-createCountry('Australia', 'autralia.png', 'AUD', [
-    { populationNbr: 25400000, year: new Date('2015-08-24') },
-   ]);
+// createCountry('Australia', 'autralia.png', 'AUD', [
+//     { populationNbr: 25400000, year: new Date('2015-08-24') },
+//    ]);
 
 
 
 
 // Create city with name, population and country foreign key
-function createCity(name, currentPopulation, countryId) { 
+function createCity(name, currentPopulation, countryId) {
+    const newCity = new City({
+        name: name,
+	    currentPopulation: currentPopulation,
+	    country: countryId,
+    })
+    newCity.save().then(data => {
+        console.log(data);})
 
 }
 // Sample call:
-// createCity('Sydney', 5312163, 'COUNTRY_ID_TO_REPLACE');
+// createCity('Sydney', 5312163, '63fc896f952caca728563e78');
 
 
 // Display country population from country name
-function displayCountryPopulation(countryName) { }
+function displayCountryPopulation(countryName) { 
+    Country.findOne({ name: countryName }).then(data => {
+		console.log('ARTICLE =>', data);
+	});
+}
+
+// displayCountryPopulation('Australia');
+
 
 
 // Display country informations from city name (with populate)
-function displayCountryFromCityName(cityName) { }
+function displayCountryFromCityName(cityName) { 
+    City.findOne({ name: cityName }).then(data => {
+		console.log('ARTICLE =>', data);
+	});
 
+}
+
+// displayCountryFromCityName("Sydney");
 
 module.exports = { createCountry, createCity, displayCountryPopulation, displayCountryFromCityName }; // Do not edit/remove this line
