bcf226015ca17c1f651fad0207d8b08f1f3792f5
Triple Patte <triplepatte123@gmail.com>
2023-02-27 10:47:32 +0100

    311

--- week3/letsmodelize/models/cities.js
+++ week3/letsmodelize/models/cities.js
@@ -1,1 +1,14 @@
 // Insert your code here
+const countrySchema = mongoose.countrySchema({
+    name: String,
+    currentPopulation : Name,
+    country: { type: mongoose.Schema.Types.ObjectId, ref: 'countries' },
+
+   
+
+});
+
+const City = mongoose.model('cities', countrySchema);
+
+
+
--- week3/letsmodelize/models/countries.js
+++ week3/letsmodelize/models/countries.js
@@ -1,1 +1,13 @@
 // Insert your code here
+const citySchema = {
+    name: String,
+    flagImg : String,
+    currency: Number,
+    population : {
+        populationNbr : Number,
+        year : Date, 
+    }
+
+}
+
+const City = mongoose.model('countries', citySchema);
