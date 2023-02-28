047141c867118b96574f2ed4ba7334d0de87552f
Triple Patte <triplepatte123@gmail.com>
2023-02-28 12:02:51 +0100

    312

--- week3/orientexpress/routes/index.js
+++ week3/orientexpress/routes/index.js
@@ -14,10 +14,18 @@ let trips = [{ departure: 'Paris', arrival: 'Lyon' }, { departure: 'Lyon', arriv
 //     "arrival": "Marseille"
 //   },
 //   {
-//     "departure": "Grenoble",
-//     "arrival": "Strasbourg"
+   
 //   }
 // ]
+
+router.post('/trips', (req, res) => {
+    trips.push({"departure": "Grenoble",
+    "arrival": "Strasbourg"});
+    res.json({
+        tripsList: trips})
+        console.log("push")
+   });
+
 
 
 // GET /trips - Sample result:
@@ -36,13 +44,31 @@ let trips = [{ departure: 'Paris', arrival: 'Lyon' }, { departure: 'Lyon', arriv
 //   }
 // ]
 
+router.get('/trips', (req, res) => {
+
+    res.json({ allTrips: trips });
+    console.log("get1")
+   });
+
 // GET /lastTrip - Sample result:
 // "lastTrip": {
 //   "departure": "Grenoble",
 //   "arrival": "Strasbourg"
 // }
 
+router.get('/lastTrip', (req, res) => {
+
+    res.json({ tripsAll: trips[trips.length-1] });
+    //console.log(trips[2])
+    console.log("get1")
+   });
+
 // DELETE /trips - Sample result:
 // "allTrips": []
+
+router.delete('/trips/:position', (req, res) => {
+    trips.splice(req.params.position, 1);
+    res.json({ deleteTrip: trips });
+   });
 
 module.exports = router;
