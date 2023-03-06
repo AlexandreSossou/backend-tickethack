a55a2ec92294a2d4ea4fefc6ec8c66fbf32d940b
Triple Patte <triplepatte123@gmail.com>
2023-03-06 12:10:26 +0100

    355

--- week3/weatherapp-part4/backend/routes/users.js
+++ week3/weatherapp-part4/backend/routes/users.js
@@ -12,10 +12,10 @@ const User = require('../models/users');
 router.post('/signup', (req, res) => {
 	// Check if the city has not already been added
 
-User.findOne({name : { $regex: new RegExp(req.body.name, 'i') }})
+User.findOne({email : { $regex: new RegExp(req.body.email, 'i') }})
 .then(data=>{
 
-    if ( req.body.name === null || req.body.name === ''){
+    if ( !req.body.email || !req.body.name ){
 
         res.json({ result: false, error: 'Missing or empty fields' });
 
@@ -40,36 +40,36 @@ User.findOne({name : { $regex: new RegExp(req.body.name, 'i') }})
 })
 });
 
+router.post('/signin', (req, res) => {
+	// Check if the city has not already been added
+
+User.findOne({email : { $regex: new RegExp(req.body.email, 'i') }})
+.then(data=>{
+
+    if ( !req.body.email || !req.body.name || req.body.name === null || req.body.email ===null){
+
+        res.json({ result: false, error: 'Missing or empty fields' });
+
+    }
+    if (data=== null ) {
+        console.log(data)
+       
+        res.json({ result: false, error: 'User not found' })
+
+    }
+    else{
+
+       
+        console.log(data)
+         res.json({ result: true});
+                  
+    }
+})
+});
 
 
 
 
-// 	User.findOne({ cityName: { $regex: new RegExp(req.body.cityName, 'i') } }).then(dbData => {
-// 		if (dbData === null) {
-// 			// Request OpenWeatherMap API for weather data
-// 			fetch(`https://api.openweathermap.org/data/2.5/weather?q=${req.body.cityName}&appid=${OWM_API_KEY}&units=metric`)
-// 				.then(response => response.json())
-// 				.then(apiData => {
-// 					// Creates new document with weather data
-// 					const newCity = new City({
-// 						cityName: req.body.cityName,
-// 						main: apiData.weather[0].main,
-// 						description: apiData.weather[0].description,
-// 						tempMin: apiData.main.temp_min,
-// 						tempMax: apiData.main.temp_max,
-// 					});
-
-// 					// Finally save in database
-// 					newCity.save().then(newDoc => {
-// 						res.json({ result: true, weather: newDoc });
-// 					});
-// 				});
-// 		} else {
-// 			// City already exists in database
-// 			res.json({ result: false, error: 'City already saved' });
-// 		}
-// 	});
-// });
 
 
 
